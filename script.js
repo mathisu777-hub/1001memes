4916e51832ab42ae7a841831127d400ab39b4c50
mathisu777-hub <mathisu777@gmail.com>
2026-03-05 15:35:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("1001 Memes loaded");
+const memes = [
+
+"https://i.imgur.com/vlV6AGm.jpeg",
+"https://i.imgur.com/WxNkK7J.jpeg",
+"https://i.imgur.com/8Km9tLL.jpeg",
+"https://i.imgur.com/9H1Z4sX.jpeg",
+"https://i.imgur.com/jv6pYyH.jpeg"
+
+];
+
+function showRandomMeme(){
+
+const randomIndex = Math.floor(Math.random() * memes.length);
+
+document.getElementById("meme").src = memes[randomIndex];
+
+}
+
+window.onload = showRandomMeme;
